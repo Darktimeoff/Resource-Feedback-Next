3ae4b0ce765d7cc9c4c9f798c9d9f42f46f60227
Yevhenii Korolikhin <alexkorolixinofficial@gmail.com>
2021-09-20 00:27:02 +0300

    clean project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps}: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps} />
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, NextScript, Main, DocumentContext, DocumentInitialProps } from 'next/document';
+
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps>{
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return {...initialProps};
+	}
+
+	render(): JSX.Element {
+		return (
+			<Html lang="ru">
+				<Head></Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
