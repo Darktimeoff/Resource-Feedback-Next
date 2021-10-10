321cc6420b5cabdc022ce0e7daab8a228858caf2
Yevhenii Korolikhin <alexkorolixinofficial@gmail.com>
2021-10-10 20:42:58 +0300

    fix style

--- page-components/TopPage/index.tsx
+++ page-components/TopPage/index.tsx
@@ -1,14 +1,17 @@
 import { HhData, Htag, Tag } from '../../components';
 import { TopPageComponentProps } from './props.component';
 import styles from './index.module.scss';
-import { IProductModel } from '../../@types';
+import { IProductModel, TopLevelCategory } from '../../@types';
 
-export const TopPageComponent = ({products, page}: TopPageComponentProps): JSX.Element => {
+export const TopPageComponent = ({products, page, firstCategory}: TopPageComponentProps): JSX.Element => {
+	const tagJSX = products && <Tag color="grey" size="m">{products.length}</Tag>;
+	const hhDataJSX = firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />;
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
-				{products && <Tag color="grey" size="m">{products.length}</Tag>}
+				{tagJSX}
 				<span>Сортировка</span>
 			</div>
 			<div>
@@ -18,7 +21,7 @@ export const TopPageComponent = ({products, page}: TopPageComponentProps): JSX.E
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
 				<Tag color="red" size="m">hh.ru</Tag>
 			</div>
-			<HhData {...page.hh} />
+			{hhDataJSX}
 		</div>
 	);
 };
