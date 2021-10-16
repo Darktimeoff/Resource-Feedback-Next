47514552974655a21589dd617e7a0c5d09b7d57c
Yevhenii Korolikhin <alexkorolixinofficial@gmail.com>
2021-10-16 17:05:59 +0300

    finish product component

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -2,7 +2,8 @@ import { ProductProps } from './props';
 import styles from './index.module.scss';
 import cn from 'classnames';
 import { Button, Card, Rating, Tag } from '..';
-import { priceRu } from '../../helpers';
+import { priceRu, declOfNum } from '../../helpers';
+import { IProductCharacteristic} from '../../@types';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	const domain = process.env.NEXT_PUBLIC_DOMAIN;
@@ -23,10 +24,12 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.ratingTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.ratingTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 			<div className={styles.hr}><hr className={styles.hrStyle}/></div>
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>feature</div>
+			<div className={styles.feature}>
+				{product.characteristics.map(characteristics)}
+			</div>
 			<div className={styles.advBlock}>
 				{
 					product.advantages && <div className={styles.advantages}>
@@ -49,3 +52,13 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 		</Card>
 	);
 };
+
+function characteristics(c: IProductCharacteristic): JSX.Element {
+	return (
+		<div className={styles.characteristics} key={c.name}>
+			<span className={styles.characteristicsName}>{c.name}</span>
+			<span className={styles.characteristicsDoted}></span>
+			<span className={styles.characteristicsValue}>{c.value}</span>
+		</div>
+	)
+}
